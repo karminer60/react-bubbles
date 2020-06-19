dc068ea8e2ca4725b612738b3705941c321a3433
karina-rodriguez <karminer60@gmail.com>
2020-06-19 10:00:05 -0700

    working on BubblePage and ColorList modules

--- client/src/axiosWithAuth.js
+++ client/src/axiosWithAuth.js
@@ -5,7 +5,7 @@ export const axiosWithAuth = () =>{
 
     return axios.create({
         headers: {
-                authorixation:token
+            authorization:token
         },
         baseURL:'http://localhost:5000'
     });
